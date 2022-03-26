554c44381ac2e05f220a96e92f5aa9fb8d5592b4
venkatesh7337 <102401930+venkatesh7337@users.noreply.github.com>
2022-03-26 10:43:32 +0530

    Add files via upload

---
+++ 03_variables.py
@@ -0,0 +1,69 @@
+# x=20
+# print(x)
+# y="iam a variable"
+# print(y)
+# z="iam also a variable"
+# print(z)
+# x=20
+# x="avariable"
+# print(x)
+# a=amer
+# print(a)
+# -4=6
+# print(-4)
+# a-b=8
+# print(a-b)
+# c_d_=9
+# print(c_d_)
+# abc=10
+# # print(abc)
+# a=5
+# A=7
+# _4=6
+# a_b=8
+# c_d=19
+# abc=10
+# print(a);print(A);print(_4);print(a_b);print(c_d);print(abc)
+# # A=5;_4=6;A=7;a_b=8;c_d=9;abc=10
+# a,b,_4,a_b,c_d,abc=5,7,6,8,9,10
+# print(a)
+# print(b)
+# print(_4)
+# print(a_b)
+# print(c_d)
+# # print(abc)
+# a=e=i=o=5
+# print(a)
+# print(e)
+# print(i)
+# # print(o)
+# n="hello"
+# m="6"
+# print(n,type(n))
+# print(m,type(m))
+# print(n,m)
+# print(n);print(m)
+# print(m);print(n)
+# a=1;b=2
+# print(a,b)
+# a=7;b=9
+# print(a)
+# print(b)
+# a=7
+# b=9
+# c=a
+# b=c
+# a=b
+# print(a)
+# print(b)
+# print(c)
+# print(b)
+# print(a)
+# print("Hello, Hi")
+# print("Hello" "Hi")
+# print(" HELL" "Hi")
+# a=1;b=2;c=3;d=2;e=4
+# print(a,b,c,d,e)
+# a,b,c,d,e,=1,2,3,4,5
+# print(a,b,c,d,e)
+
