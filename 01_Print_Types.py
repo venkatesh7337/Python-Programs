07d98cef418754d9eb2553527efbdcf592133de3
akhilareddy456 <102400626+akhilareddy456@users.noreply.github.com>
2022-03-26 09:47:20 +0530

    Add files via upload

---
+++ 01_Print_Types.py
@@ -0,0 +1,44 @@
+# print('hello python')
+# print("hello python")
+
+# print('hello worl"d') 
+# print("hello pyt'hon")
+
+# print('''Hello
+# World''')
+
+# print('''Hello
+#          World''')
+
+# print('''Hello 
+
+#         World''')
+
+# print("""Hello
+
+#          World""")
+
+# print('''hello python''')
+# print("""hello python""")
+
+# print("hello py'thon")  
+# print('''hello py'thon''')
+# print("""hello py'thon""") 
+
+# print('print(hello python)')
+# print("print(hello python)")
+# print('''print(hello python)''')
+# print("""print(hello python)""")
+
+# print('hello' 'world') 
+# print('hello'  ,     'world') 
+
+# print("Python " * 5)
+# print(5 * "Python ")
+
+# print('4 ' *4)
+# print('hello python3 '*2)
+
+# print('python'*3.4)
+
+# print('python'* 'core')
